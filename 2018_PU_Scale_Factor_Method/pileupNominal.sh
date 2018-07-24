e944166ce57dfc5c608f1651a3ab485d844fcbbb
Jamal <jrorie@rice.edu>
2018-07-24 23:46:29 +0200

    Added scripts

---
+++ 2018_PU_Scale_Factor_Method/pileupNominal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+JSON="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco//Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt"
+PUJSON="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt"
+
+pileupCalc.py -i $JSON --inputLumiJSON $PUJSON  --calcMode true --minBiasXsec 69200 --maxPileupBin 100 --numPileupBins 100  --pileupHistName=PUpileupNominal pileupNominal.root
+
